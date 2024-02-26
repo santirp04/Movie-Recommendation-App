68aa3fa74649900cbd27f9c6072c2682d1626d0a
Santiago Ramirez <santirp04@gmail.com>
2024-02-26 11:54:46 -0600

    Update API authentication in api.py

--- api.py
+++ api.py
@@ -1,11 +1,12 @@
 import requests
+from creds import key
 
 def fetchData(name):
     # The headers are required to authenticate the request
     creds = ...  # Define the variable "creds" with the appropriate value
     headers = {
         "accept": "application/json",
-        "Authorization": "Bearer " + creds
+        "Authorization": "Bearer " + key()
     }
     response = requests.get("https://api.themoviedb.org/3/search/movie?query="+name+"&include_adult=true&language=en-US&page=1", headers=headers)
 
